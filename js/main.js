1707382477f177d3f794eb57cf9eeb60d07ac2a3
Home_System <martin.fobert@gmail.com>
2024-11-04 21:57:27 -0500

    updated portfolio
    updated portfolio site. added JS file and functions

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// portfolio website script
+
+function showContactBox() {
+  document.getElementById("contact-section").classList.remove("hide");
+}
+
+function hideContactBox() {
+  document.getElementById("contact-section").classList.add("hide");
+}
