30c3de96592c1f8d0729b7e3b5bd989be9663bbc
Home_System <martin.fobert@gmail.com>
2024-11-04 22:21:20 -0500

    added comments
    added comments to JS and HTML

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 // portfolio website script
 
-function showContactBox() {
+function showContactBox() { // this functions removes the "hide" class from the contact section making the element visable
   document.getElementById("contact-section").classList.remove("hide");
 }
 
-function hideContactBox() {
+function hideContactBox() { // this function adds the hide element to the class of the contact section setting it's display to none
   document.getElementById("contact-section").classList.add("hide");
 }
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Portfolio Assignment Repository
+## This portfolio is a working progress
+This is a single page website demonstrating some basic HTML, CSS and JavaScript code
